815da2434a6d3975e81f0463024e9d5a126f2eba
Dmytro Parzhytskyi <parzhitsky@gmail.com>
2024-03-20 23:46:40 +0200

    Reimplement `Broker` to be more strict and straightforward

--- packages/playground/src/message-queue/consumer.ts
+++ packages/playground/src/message-queue/consumer.ts
@@ -5,9 +5,24 @@ export abstract class Consumer<const out Type extends MessageType, Data> {
 
   protected abstract doConsume(message: Message<Type, Data>): void | PromiseLike<void>
 
+  canConsume(message: Message<MessageType, unknown>): boolean {
+    return message.type === this.messageType
+  }
+
   async consume(message: Message<Type, Data>): Promise<void> {
-    if (message.type === this.messageType) {
-      await this.doConsume(message)
+    if (message.type !== this.messageType) {
+      throw new MessageTypeMismatchError(message.type, this.messageType)
     }
+
+    await this.doConsume(message)
+  }
+}
+
+export class MessageTypeMismatchError extends Error {
+  constructor(
+    public readonly receivedMessageType: MessageType,
+    public readonly expectedMessageType: MessageType,
+  ) {
+    super(`Received message type "${receivedMessageType}" is not equal to expected message type "${expectedMessageType}"`)
   }
 }
