32895e0e5ea700b00bc32833726bc6bd245ecdde
Dmytro Parzhytskyi <parzhitsky@gmail.com>
2024-03-20 23:51:30 +0200

    Rename to `MessageTypeToDataMapUnknown`

--- packages/playground/src/message-queue/message.type.ts
+++ packages/playground/src/message-queue/message.type.ts
@@ -2,9 +2,9 @@ export type MessageType = string // encapsulation
 
 export { MessageType as Type } // alias
 
-export type MessageTypeToDataMapAny = Record<MessageType, unknown>
+export type MessageTypeToDataMapUnknown = Record<MessageType, unknown>
 
-export type MessageTypeOf<Map extends MessageTypeToDataMapAny> = Extract<keyof Map, MessageType>
+export type MessageTypeOf<Map extends MessageTypeToDataMapUnknown> = Extract<keyof Map, MessageType>
 
 export interface Message<out Type extends MessageType, Data> {
   readonly type: Type
